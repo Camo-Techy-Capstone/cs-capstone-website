c048c2c2a47473a3078637a7589538fcd4dde181
Dima <Dima.m99@gmail.com>
2021-07-29 15:04:21 -0700

    Add React-player, temp content

--- 2021-AUG/src/Views/Donors.tsx
+++ 2021-AUG/src/Views/Donors.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Container from "@material-ui/core/Container";
+import ReactPlayer from 'react-player/vimeo';
+
 
 const Donors = () => {
-    return <h1>TODO: Donor Page</h1>;
+    return (
+        <Container maxWidth="lg">
+            <h1>Donors</h1>
+            <p>We Would also like to give a special thank you to Checkfront for being a major sponsor of this event. Thank you!</p>
+            <ReactPlayer style={{margin: "auto"}} controls={true} url='https://vimeo.com/570531995' />
+        </Container>
+    );
 }
 
 export default Donors;
