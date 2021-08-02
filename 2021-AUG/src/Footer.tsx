9c1bd713bd1deb59f0fbff4cd9fafbcd582ffff4
Seiji Sagawa <seiji.rb.tr0327@gmail.comm>
2021-08-01 22:32:06 -0700

    Added Footer that displays the Camosun logo and copyright.

---
+++ 2021-AUG/src/Footer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { AppBar, Box, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    height: 100,
+    width: 180,
+    objectFit: "scale-down", 
+  },
+  footer: {
+      top: 'auto',
+      bottom: 0,
+  }
+}));
+
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position='static'>
+        <Box textAlign="center">
+            <img src="/cosc/2021/images/Camosun_Logo.png" className={classes.logo}/>
+        </Box>
+        <Box textAlign="center">
+            <Typography>
+                &copy; {new Date().getFullYear()}: Camosun College
+            </Typography>
+        </Box>
+        
+    </AppBar>
+  );
+};
+
+export default Footer;
--- 2021-AUG/src/Layout.tsx
+++ 2021-AUG/src/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import { Box } from "@material-ui/core";
 
 const Layout: React.FC = (props: PropsWithChildren<any>) => {
@@ -8,6 +9,7 @@ const Layout: React.FC = (props: PropsWithChildren<any>) => {
     <div>
       <Header />
       <Box pt={10}>{children}</Box>
+      <Footer />
     </div>
   );
 };
