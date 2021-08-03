b358c23c427c044d1ed937a2a6ffbd467fa8d0e0
Dimitry (Dima) Maizik <dima.m99@gmail.com>
2021-08-03 15:21:37 -0700

    Remove Video, Change Phrasing

--- 2021-AUG/src/Views/Donors.tsx
+++ 2021-AUG/src/Views/Donors.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Container from "@material-ui/core/Container";
-import ReactPlayer from 'react-player/vimeo';
-
 
 const Donors = () => {
     return (
         <Container maxWidth="lg">
             <h1>Donors</h1>
-            <p>We Would also like to give a special thank you to Checkfront for being a major sponsor of this event. Thank you!</p>
-            <ReactPlayer style={{margin: "auto"}} controls={true} url='https://vimeo.com/570531995' />
+            <p>We would like to give a huge thank you the people and companies who have donated to this years capstone symposium! We could not have done this without your support. Thank you!</p>
+            <p>We would also like to give a special thanks to Checkfront for being a major sponsor of this event.</p>
         </Container>
     );
 }
